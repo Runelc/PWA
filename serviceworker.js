b8030939b7e12d39a732054ce603b138d48d0755
Rune <rune1833@live.dk>
2022-10-04 09:18:09 +0200

    Rettelse serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,20 @@
-const cacheName = 'cache-insects';
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(["insects/", "/insects/index.html", "/insects/butterflies.jpg", 
-    "/insects/butterfly.jpg", "/insects/dragonfly.jpg"]);
-    })
-  );
-});
+const cacheName = "cache-insects";
 
-/* Hvis ressourcen ikke er tilgængelig online, så søg i cahcehn efter et match  */
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.open(cacheName).then(cache => cache.match(event.request))
+// Når websitet indlæses, cache ressourcer nævnt i listen
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(["/insects2/", "/insects2/index.html", "/insects2/butterflies.jpg", "/insects2/butterfly.jpg", "/insects2/dragonfly.jpg"]);
+        })
     )
-  );
-}); 
+})
+
+// Hvis ressource ikke er tilgængelig online, så søg i cachen
+// efter et match.
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        fetch(event.request).catch(() =>
+        caches.open(cacheName).then(cache=>cache.match(event.request))
+        )
+    )
+})
